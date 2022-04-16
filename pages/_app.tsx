44d8b5a812e6ed36ea7ded21355c5599c0c38ed5
Kevin Nguyen <me@kndwin.dev>
2022-04-16 15:52:27 +1000

    🧹 chore: adding tracking with umami

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import Script from "next/script";
 import { globalStyles } from "stitches.config";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <Script
+          async
+          defer
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+          src={process.env.NEXT_PUBLIC_UMAMI_SRC_URL}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
