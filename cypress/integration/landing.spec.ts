faa8828b7022eac66b538dee8adf64672d609759
Kevin Nguyen <me@kndwin.dev>
2022-08-03 01:15:38 +1000

    🧹 chore: updated test case to reflect home page changes

--- cypress/integration/landing.spec.ts
+++ cypress/integration/landing.spec.ts
@@ -2,6 +2,6 @@ export {};
 describe("Show landing page", () => {
   it("show landing page", () => {
     cy.visit("http://localhost:3000/");
-    cy.get("h1").should("contain", "Higher");
+    cy.get("h1").should("contain", "Read");
   });
 });
